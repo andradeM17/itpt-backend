699778d3660a6f5160a3fe7440bfe6b0a795e713
andradeM17 <andradem@tcd.ie>
2026-06-15 16:26:47 +0100

    punkt_tab for splitter

--- tools/splitter.py
+++ tools/splitter.py
@@ -1,5 +1,6 @@
 import nltk
-nltk.download("punkt", quiet=True)
+#nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 
 def split_sentences(text: str):
     return nltk.sent_tokenize(text)
