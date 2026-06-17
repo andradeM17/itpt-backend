3a7499b993ed4412ba29f6c6428159654c2cb018
andradeM17 <andradem@tcd.ie>
2026-06-17 10:02:23 +0100

    added Irish language model

--- tools/detector.py
+++ tools/detector.py
@@ -1,5 +1,12 @@
-from langdetect import detect, DetectorFactory
+import os
+from langdetect import DetectorFactory, detector_factory
+
+PROFILE_DIR = os.path.join(os.path.dirname(__file__), "custom_profiles")
+
+detector_factory.load_profiles(PROFILE_DIR)
 DetectorFactory.seed = 0
 
 def detect_language(text: str) -> str:
-    return detect(text)
+    detector = detector_factory.create()
+    detector.append(text)
+    return detector.detect()
