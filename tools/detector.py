998a77794e6b61737eeae645a1e8251727cd07a3
andradeM17 <andradem@tcd.ie>
2026-06-17 10:15:25 +0100

    entire langdetect added

--- tools/detector.py
+++ tools/detector.py
@@ -1,12 +1,5 @@
-import os
-from langdetect import DetectorFactory, detector_factory
-
-PROFILE_DIR = os.path.join(os.path.dirname(__file__), "custom_profiles")
-
-detector_factory.load_profiles(PROFILE_DIR)
+from tools.langdetect import detect, DetectorFactory
 DetectorFactory.seed = 0
 
 def detect_language(text: str) -> str:
-    detector = detector_factory.create()
-    detector.append(text)
-    return detector.detect()
+    return detect(text)
